b45cea2e55d02691094d7eec25c34baa747ed68f
rawanshisht <rawan.shisht@gmail.com>
2023-10-15 05:52:24 +0100

    fix build issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/AppBar/AppBar";
 import NotFound from "./components/NotFound/NotFound";
 import Home from "./components/HomePage/HomePage";
 import Main from "./components/Main/MainPage";
